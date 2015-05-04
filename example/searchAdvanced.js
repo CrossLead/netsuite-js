14e93b9ca31eaa01339fa804af9162993f1d3dcf
Christian Yang <yang.christian@gmail.com>
2015-05-03 18:10:04 -0700

    Minimal advanced search (CustomerSearchAdvanced), some more basic searches - TODO: CustomerSearchRow columns, CustomerSearch criteria - Fix some serializer bugs

---
+++ example/searchAdvanced.js
@@ -0,0 +1,50 @@
+/*
+ * netsuite-js
+ * https://github.com/CrossLead/netsuite-js
+ *
+ * Copyright (c) 2015 Christian Yang
+ * Licensed under the Apache license.
+ */
+
+'use strict';
+
+var denodeify = require('denodeify');
+var NetSuite = require('../');
+
+var credentials = require('./credentials.json');
+var config = new NetSuite.Configuration(credentials);
+var service = new NetSuite.Service(config);
+
+console.log('Creating NetSuite connection');
+
+service
+  .init(true /* skipDiscovery */ )
+  .then(function( /*client*/ ) {
+    console.log('WSDL processed');
+
+    var preferences = new NetSuite.Search.SearchPreferences();
+    preferences.pageSize = 45;
+    preferences.bodFieldsOnly = false;
+    preferences.returnSearchColumns = false;
+    service.setSearchPreferences(preferences);
+
+    var search = new NetSuite.Search.CustomerSearchAdvanced();
+
+    console.log('Performing default CustomerSearchAdvanced');
+    return service.search(search);
+  })
+  .then(function(result, raw, soapHeader) {
+    if (result.searchResult.status.$attributes.isSuccess !== 'true') {
+      console.error('Error');
+      console.error(result.searchResult.status.statusDetail);
+    }
+    console.log('Records found: ' + result.searchResult.totalRecords);
+    console.log(JSON.stringify(result.searchResult.recordList, null, 2));
+    console.log('Last Request:');
+    console.log(service.config.client.lastRequest);
+  })
+  .catch(function(err) {
+    console.error(err);
+    console.error('Last Request:');
+    console.error(service.config.client.lastRequest);
+  });
